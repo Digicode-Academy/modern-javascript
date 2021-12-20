ed553ed846c2f5564938ee2f0fce1c6eec7660a0
Ajat Darojat <ajatdarojat45@gmail.com>
2021-12-21 06:33:19 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Naming variable:
+ * 1. Don't use reserved keyword
+ * 2. Don't use symbols #, @
+ * 3. Don't use space
+ * 4. Don't start with number
+ */
+
+let title = "Naming variable"
+let my_country = "Indonesia" // snake case
+let myCountry = "Indonesia" // camle case
+let MyCountry = "Indonesia" // pascal case
