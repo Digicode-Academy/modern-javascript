153a576d7fef937c741112ac8f3bfefd2a8659fc
Ajat Darojat <ajatdarojat45@gmail.com>
2021-12-23 11:28:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,23 @@
-// create an array
-let students = ["Udin", "Kosasih", "Jhon"];
-console.log(students);
+// how to create an object #1
+const car = new Object();
 
-let numbers = [1, 2, 3, 4];
-console.log(numbers);
+// add property to existing object
+car.merk = "BMW";
+car.color = "red";
+car.price = 10000;
+console.log(car);
 
+// how to create an object #2
+const cup = {
+	colors: ["yellow", "red"],
+	weight: 10,
+	height: 10,
+	user: {
+		name: "Udin",
+		age: 19,
+	},
+};
+console.log(cup);
 
-// get length of array
-console.log(students.length);
-
-// get specific item on array by index
-console.log(students[0]);
-
-// Add an item to the end of an Array
-students.push("Doe");
-console.log(students);
-
-// Remove an item from the end of an Array
-students.pop();
-console.log(students);
-
-// Multidimensional array
-let board = [
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-	[1, 2, 3, 4, 5, 6, 7, 8],
-];
-
-console.log(board);
+// access specific data in object
+console.log(cup.height);
